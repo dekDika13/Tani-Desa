1a6ca9ab029704c7ec55ea3e30c95eaaadf95409
dika2015091057 <dika.rama@undiksha.ac.id>
2022-12-27 04:26:36 +0800

    update dto product

--- dto/adminDto/product.go
+++ dto/adminDto/product.go
@@ -2,14 +2,14 @@ package adminDto
 
 type ProductDTO struct {
 	ProductID   uint   `json:"product_id"`
-	Name        string `json:"name" validate:"required"`
+	Name        string `json:"name"`
 	Image       string `json:"image"`
-	Type        string `json:"type" validate:"required"`
+	Type        string `json:"type"`
 	Qty         uint   `json:"qty"`
-	Price       uint   `json:"price" validate:"required"`
+	Price       uint   `json:"price"`
 	Description string `json:"description"`
 	Address     string `json:"address"`
-	Owner       string `json:"owner" validate:"required"`
+	Owner       string `json:"owner"`
 	AdminName   string `json:"admin_name"`
 }
 
